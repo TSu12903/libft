a197ef14797a88ce9fdc4adf6e7fe2e9ad469347
Timothy Cybak <tcybak@2F3.42angouleme.fr>
2024-10-14 15:46:57 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -14,10 +14,73 @@
 
 char	**ft_split(char const *s, char c)
 {
-	int		i;
-	int		j;
-	char	*str;
+	size_t		i;
+	size_t		j;
+	char	**str;
+	size_t		t;
+	size_t		h;
 
+	i = 0;
+	t = 0;
 	j = ft_strlen(s);
-	
+	while (i < j)
+	{
+		if (s[i] == c)
+			i++;
+		if (s[i] != c)
+		{
+			while (s[i] != c)
+				i++;
+			t++;
+		}
+		if (s[i] == '\0' && t == 0)
+			return (ft_calloc(1,sizeof(char*)));
+	}
+	str = ft_calloc((t + 1), sizeof(char*));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (j < t)
+	{
+		while (s[i] == c )
+			i++;
+		if (s[i] != c)
+		{
+			h = i;
+			while (s[h] != c && s[h])
+			{
+				h++;
+			}
+			str[j] = ft_substr(s,i,(h - i));
+			if (str[j] == 0)
+			{
+				while (j > 0)
+				{
+					free(str[j]);
+					j--;
+				}
+			free(str);
+			}
+			j++;
+		}
+		i = h;
+	}
+	return (str);
 }
+/*
+int	main(void)
+{
+	char	**str;
+	size_t	i;
+	char	c;
+	c = ' ';
+	i = 0;
+	str = ft_split("    hh Hhhh qjdoqw ", c);
+	while (str[i])
+	{
+		printf("%s---", str[i]);
+		i++;
+	}
+}
+*/
